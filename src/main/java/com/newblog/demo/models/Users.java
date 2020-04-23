ce4450249ef19806cc68f7e580bf698c4190dd6c
Matthew <murphy.d.matthew28@gmail.com>
2020-04-23 12:22:31 -0500

    'updated'

--- src/main/java/com/newblog/demo/models/Users.java
+++ src/main/java/com/newblog/demo/models/Users.java
@@ -1,9 +1,12 @@
 package com.newblog.demo.models;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "users")
 public class Users {
+    @Id
+
 }
