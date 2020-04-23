c304549879efcdf514203ffbdcc20f661a4bfb3d
Matthew <murphy.d.matthew28@gmail.com>
2020-04-23 17:11:03 -0500

    'updated'

--- src/main/java/com/newblog/demo/models/Users.java
+++ src/main/java/com/newblog/demo/models/Users.java
@@ -9,4 +9,5 @@ public class Users {
 @GeneratedValue(strategy = GenerationType.IDENTITY)
 @Column(nullable = false, columnDefinition = "INT(10) UNSIGNED")
 private long id;
+@Column(nullable = false, )
 }
