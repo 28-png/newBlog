1a641682b265a126ce4318dd355b736884d3e201
Matthew <murphy.d.matthew28@gmail.com>
2020-04-23 17:18:03 -0500

    'updated'

--- src/main/java/com/newblog/demo/models/Users.java
+++ src/main/java/com/newblog/demo/models/Users.java
@@ -5,11 +5,12 @@ import javax.persistence.*;
 @Entity
 @Table(name = "users")
 public class Users {
-    @Id
+@Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
 @Column(nullable = false, columnDefinition = "INT(10) UNSIGNED")
 private long id;
 @Column(nullable = false, unique = true)
 private String email;
 @Column(nullable = false)
+private String password;
 }
