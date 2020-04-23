9c5a7710282ed7cf1c1765c4ac315568880e0281
Matthew <murphy.d.matthew28@gmail.com>
2020-04-23 12:23:25 -0500

    'updated'

--- src/main/java/com/newblog/demo/models/Users.java
+++ src/main/java/com/newblog/demo/models/Users.java
@@ -1,12 +1,10 @@
 package com.newblog.demo.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class Users {
     @Id
-
+@GeneratedValue(strategy = GenerationType.IDENTITY)
 }
