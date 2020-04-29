a4a83c64f37641eec060ac78dfa6155729975145
Matt Murphy <mattmurphy@matts-air.lan>
2020-04-29 10:33:08 -0500

    updated

--- src/main/java/com/newblog/demo/models/Favorites.java
+++ src/main/java/com/newblog/demo/models/Favorites.java
@@ -1,10 +1,12 @@
 package com.newblog.demo.models;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "favorites")
 public class Favorites {
+@Id
 
 }
