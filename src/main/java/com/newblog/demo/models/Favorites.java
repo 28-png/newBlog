4e5e63344a8e577385fac1089e0beea7ee40cda4
Matt Murphy <mattmurphy@matts-air.lan>
2020-04-29 11:20:52 -0500

    updated

--- src/main/java/com/newblog/demo/models/Favorites.java
+++ src/main/java/com/newblog/demo/models/Favorites.java
@@ -12,12 +12,50 @@ private long id;
 private String description;
 @Column(nullable = false)
 private String body;
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private Users user;
+@ManyToOne
+@JoinColumn(name = "user_id")
+private Users user;
+@ManyToOne
+@JoinColumn(name = "blog_id")
+private Blog blog;
 
-    @ManyToOne
-    @JoinColumn(name = "blog_id")
-    private Blog blog;
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
+
+    public Blog getBlog() {
+        return blog;
+    }
+
+    public void setBlog(Blog blog) {
+        this.blog = blog;
+    }
 }
