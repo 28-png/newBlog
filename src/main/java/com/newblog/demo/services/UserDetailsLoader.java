5b686f6e09cedd970f1d59e77af076817c050914
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-05-04 10:04:44 -0500

    updated

--- src/main/java/com/newblog/demo/services/UserDetailsLoader.java
+++ src/main/java/com/newblog/demo/services/UserDetailsLoader.java
@@ -1,7 +1,8 @@
 package com.newblog.demo.services;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserDetailsLoader {
+public class UserDetailsLoader implements UserDetailsService {
 }
