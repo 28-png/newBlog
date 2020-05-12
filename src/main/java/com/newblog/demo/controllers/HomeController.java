ca82b435c87611ca0f4bed9276b750d461bd0a51
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-05-12 05:11:05 -0500

    updated

--- src/main/java/com/newblog/demo/controllers/HomeController.java
+++ src/main/java/com/newblog/demo/controllers/HomeController.java
@@ -1,8 +1,14 @@
 package com.newblog.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+@GetMapping("/")
+public String landingPage() {
+    return "landing";
+}
 
 }
