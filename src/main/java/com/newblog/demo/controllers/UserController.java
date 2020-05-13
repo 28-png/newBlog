53b1e7e5059486e190b8718f5a57ade3157bab23
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-05-13 15:42:42 -0500

    updated

--- src/main/java/com/newblog/demo/controllers/UserController.java
+++ src/main/java/com/newblog/demo/controllers/UserController.java
@@ -1,9 +1,12 @@
 package com.newblog.demo.controllers;
 
+import com.newblog.demo.repo.UserRepo;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserController {
+    private UserRepo usersdao;
+
 
 
 
--- src/main/java/com/newblog/demo/repo/UserRepo.java
+++ src/main/java/com/newblog/demo/repo/UserRepo.java
@@ -1,12 +1,12 @@
-//package com.newblog.demo.repo;
-//
-//import com.newblog.demo.models.Blog;
-//import com.newblog.demo.models.Users;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface UserRepo extends JpaRepository<Users, Long> {
-//    Users findByUsername(String users);
-//    Users findByUserRole(String users);
-//    Users findByUserRoleAndId(String users, long id);
-//    Users findUserById(long id);
-//}
+package com.newblog.demo.repo;
+
+import com.newblog.demo.models.Blog;
+import com.newblog.demo.models.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<Users, Long> {
+    Users findByUsername(String users);
+    Users findByUserRole(String users);
+    Users findByUserRoleAndId(String users, long id);
+    Users findUserById(long id);
+}
