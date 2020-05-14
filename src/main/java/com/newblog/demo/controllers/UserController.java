3ce51a5d94a9e7d006c1bfbcb2afd8aa4da307ad
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-05-14 15:39:46 -0500

    updated

--- src/main/java/com/newblog/demo/controllers/UserController.java
+++ src/main/java/com/newblog/demo/controllers/UserController.java
@@ -1,7 +1,10 @@
 package com.newblog.demo.controllers;
 
+import com.newblog.demo.models.Users;
 import com.newblog.demo.repo.UserRepo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
@@ -11,7 +14,11 @@ public UserController(UserRepo users) {
 this.usersdao = users;
 }
 
-
+@GetMapping("/signup")
+public String userSignupForm(Model model) {
+    model.addAttribute("user", new Users());
+    return "users/signup";
+}
 
 
 
